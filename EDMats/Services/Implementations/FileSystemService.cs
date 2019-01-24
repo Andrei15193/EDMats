ce623c47115877970b74db87087c38127e42f4a0
Andrei Fangli <andrei_fangli@outlook.com>
2019-01-24 16:45:10 +0200

    Fixed bug when application crashed due to ED writing to journal file

--- EDMats/Services/Implementations/FileSystemService.cs
+++ EDMats/Services/Implementations/FileSystemService.cs
@@ -6,7 +6,7 @@ namespace EDMats.Services.Implementations
     public class FileSystemService : IFileSystemService
     {
         public TextReader OpenRead(string fileName)
-            => new StreamReader(fileName, Encoding.UTF8);
+            => new StreamReader(new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite), Encoding.UTF8, false, 2048, false);
 
         public TextWriter OpenWrite(string fileName)
             => new StreamWriter(fileName, false, Encoding.UTF8);
