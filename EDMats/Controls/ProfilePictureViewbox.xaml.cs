ca885d4eafcb76db8b2d86605bf21ab3ab214651
Andrei Fangli <andrei_fangli@outlook.com>
2021-06-27 18:36:19 +0300

    Added more profile pictures (each ship + ED logo are now available)

--- EDMats/Controls/ProfilePictureViewbox.xaml.cs
+++ EDMats/Controls/ProfilePictureViewbox.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Shapes;
@@ -8,6 +9,47 @@ namespace EDMats.Controls
 {
     public partial class ProfilePictureViewbox : Viewbox
     {
+        private static readonly IReadOnlyDictionary<ProfilePicture, string> _drawingResourceKeys = new Dictionary<ProfilePicture, string>()
+        {
+            { ProfilePicture.Adder, "Drawings_Adder" },
+            { ProfilePicture.AllianceChallenger, "Drawings_AllianceChallenger" },
+            { ProfilePicture.AllianceChieftain, "Drawings_AllianceChieftain" },
+            { ProfilePicture.AllianceCrusader, "Drawings_AllianceCrusader" },
+            { ProfilePicture.Anaconda, "Drawings_Anaconda" },
+            { ProfilePicture.AspExplorer, "Drawings_AspExplorer" },
+            { ProfilePicture.AspScout, "Drawings_AspScout" },
+            { ProfilePicture.Beluga, "Drawings_Beluga" },
+            { ProfilePicture.CobraMk3, "Drawings_CobraMk3" },
+            { ProfilePicture.DiamondbackExplorer, "Drawings_DiamondbackExplorer" },
+            { ProfilePicture.DiamondbackScout, "Drawings_DiamondbackScout" },
+            { ProfilePicture.Dolphin, "Drawings_Dolphin" },
+            { ProfilePicture.EagleMk2, "Drawings_EagleMk2" },
+            { ProfilePicture.FederalAssaultShip, "Drawings_FederalAssaultShip" },
+            { ProfilePicture.FederalCorvette, "Drawings_FederalCorvette" },
+            { ProfilePicture.FederalDropship, "Drawings_FederalDropship" },
+            { ProfilePicture.FederalGunship, "Drawings_FederalGunship" },
+            { ProfilePicture.FerDeLance, "Drawings_FerDeLance" },
+            { ProfilePicture.Hauler, "Drawings_Hauler" },
+            { ProfilePicture.ImperialClipper, "Drawings_ImperialClipper" },
+            { ProfilePicture.ImperialCourier, "Drawings_ImperialCourier" },
+            { ProfilePicture.ImperialCutter, "Drawings_ImperialCutter" },
+            { ProfilePicture.ImperialEagle, "Drawings_ImperialEagle" },
+            { ProfilePicture.Keelback, "Drawings_Keelback" },
+            { ProfilePicture.KraitMk2, "Drawings_KraitMk2" },
+            { ProfilePicture.KraitPhantom, "Drawings_KraitPhantom" },
+            { ProfilePicture.Mamba, "Drawings_Mamba" },
+            { ProfilePicture.Orca, "Drawings_Orca" },
+            { ProfilePicture.Python, "Drawings_Python" },
+            { ProfilePicture.Type10, "Drawings_Type10" },
+            { ProfilePicture.Type6, "Drawings_Type6" },
+            { ProfilePicture.Type7, "Drawings_Type7" },
+            { ProfilePicture.Type9, "Drawings_Type9" },
+            { ProfilePicture.ViperMk3, "Drawings_ViperMk3" },
+            { ProfilePicture.ViperMk4, "Drawings_ViperMk4" },
+            { ProfilePicture.Vulture, "Drawings_Vulture" },
+            { ProfilePicture.Sidewinder, "Drawings_Sidewinder" }
+        };
+
         public ProfilePictureViewbox()
         {
             InitializeComponent();
@@ -32,24 +74,16 @@ namespace EDMats.Controls
 
         private void _SetProfilePicture()
         {
-            switch (Picture)
-            {
-                case ProfilePicture.Sidewinder:
-                    Child = (UIElement)Resources["Drawings_Sidewinder"];
-                    break;
-
-                case ProfilePicture.EliteDangerousLogo:
-                    Child = new Path
-                    {
-                        Fill = (Brush)Resources["GoldBrush"],
-                        Data = (Geometry)Resources["Elite_Dangerous_Logo"]
-                    };
-                    break;
-
-                default:
-                    Child = null;
-                    break;
-            }
+            if (Picture == ProfilePicture.EliteDangerousLogo)
+                Child = new Path
+                {
+                    Fill = (Brush)FindResource("GoldBrush"),
+                    Data = (Geometry)FindResource("Elite_Dangerous_Logo")
+                };
+            else if (_drawingResourceKeys.TryGetValue(Picture, out var drawingResourceKey))
+                Child = (UIElement)FindResource(drawingResourceKey);
+            else
+                Child = null;
         }
     }
 }
